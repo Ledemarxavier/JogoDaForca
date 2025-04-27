3e440d9aafce2974df01916c7dba00a86757bc3e
Ledemarxavier <ledemarxdeoliveira@gmail.com>
2025-04-27 14:31:51 -0300

    Código organizado com Classes e métodos.

--- JogoDaForca.ConsoleApp/Jogo.cs
+++ JogoDaForca.ConsoleApp/Jogo.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JogoDaForca.ConsoleApp
 {
@@ -13,6 +10,26 @@ namespace JogoDaForca.ConsoleApp
 
         public void Iniciar()
         {
+            while (true)
+            {
+                Console.Clear();
+                MostrarCabecalho();
+
+                string categoriaEscolhida = entrada.ObterCategoria();
+                string[] palavras = ObterPalavras(categoriaEscolhida);
+
+                if (palavras == null)
+                {
+                    Console.WriteLine("Categoria inválida! Pressione Enter para tentar novamente.");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                JogarPartida(palavras, categoriaEscolhida);
+
+                if (!entrada.DesejaContinuar())
+                    break;
+            }
         }
 
         public string[] ObterPalavras(string categoria)
@@ -44,6 +61,97 @@ namespace JogoDaForca.ConsoleApp
             char[] letrasEncontradas = new char[palavraEscolhida.Length];
             for (int i = 0; i < letrasEncontradas.Length; i++)
                 letrasEncontradas[i] = '_';
+
+            List<char> letrasChutadas = new List<char>();
+            int quantidadeErros = 0;
+            bool jogadorAcertou = false;
+            bool jogadorEnforcou = false;
+
+            do
+            {
+                DesenhoForca(quantidadeErros, categoriaEscolhida, letrasChutadas, letrasEncontradas);
+
+                string tentativa = entrada.ObterTentativa();
+
+                if (tentativa.Length > 1)
+                {
+                    jogadorAcertou = tentativa == palavraEscolhida;
+                    if (!jogadorAcertou) quantidadeErros++;
+                }
+                else
+                {
+                    ProcessarLetra(tentativa[0], palavraEscolhida, letrasChutadas, letrasEncontradas, ref quantidadeErros);
+                    jogadorAcertou = new string(letrasEncontradas) == palavraEscolhida;
+                }
+
+                jogadorEnforcou = quantidadeErros > 5;
+            } while (!jogadorAcertou && !jogadorEnforcou);
+
+            MostrarResultado(jogadorAcertou, palavraEscolhida);
+        }
+
+        private void DesenhoForca(int erros, string categoria, List<char> chutes, char[] letras)
+        {
+            string cabeca = erros >= 1 ? " o " : " ";
+            string tronco = erros >= 2 ? "x" : " ";
+            string troncoBaixo = erros >= 2 ? " x " : " ";
+            string bracoEsquerdo = erros >= 3 ? "/" : " ";
+            string bracoDireito = erros >= 4 ? "\\" : " ";
+            string pernas = erros >= 5 ? "/ \\" : " ";
+
+            Console.Clear();
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("Jogo da Forca");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine(" ___________        ");
+            Console.WriteLine(" |/        |        ");
+            Console.WriteLine(" |        {0}       ", cabeca);
+            Console.WriteLine(" |        {0}{1}{2} ", bracoEsquerdo, tronco, bracoDireito);
+            Console.WriteLine(" |        {0}       ", troncoBaixo);
+            Console.WriteLine(" |        {0}       ", pernas);
+            Console.WriteLine(" |                  ");
+            Console.WriteLine(" |                  ");
+            Console.WriteLine("_|____              ");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("Categoria escolhida: " + categoria);
+            Console.WriteLine("Erros do jogador: " + erros);
+            Console.WriteLine("Letras já chutadas: " + string.Join(", ", chutes));
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("Palavra escolhida: " + new string(letras));
+            Console.WriteLine("----------------------------------------------");
+        }
+
+        private void ProcessarLetra(char chute, string palavra, List<char> chutes, char[] letras, ref int erros)
+        {
+            if (chutes.Contains(chute))
+            {
+                Console.WriteLine("Você já chutou essa letra. Pressione Enter para continuar.");
+                Console.ReadLine();
+                return;
+            }
+
+            chutes.Add(chute);
+            bool acertou = false;
+
+            for (int i = 0; i < palavra.Length; i++)
+            {
+                if (palavra[i] == chute)
+                {
+                    letras[i] = chute;
+                    acertou = true;
+                }
+            }
+
+            if (!acertou) erros++;
+        }
+
+        public void MostrarResultado(bool venceu, string palavra)
+        {
+            Console.Clear();
+            if (venceu)
+                Console.WriteLine("Parabéns! Você acertou a palavra " + palavra);
+            else
+                Console.WriteLine("Você perdeu! A palavra era " + palavra);
         }
     }
 }
--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -1,108 +1,15 @@
-﻿using System;
+﻿using JogoDaForca.ConsoleApp;
+using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 
-internal class Program
+namespace JogoDaForca
 {
-    private static void Main()
+    public class Program
     {
-        // Sorteia uma palavra da categoria escolhida
-        Random random = new Random();
-        string palavraEscolhida = palavras[random.Next(palavras.Length)];
-
-        char[] letrasEncontradas = new char[palavraEscolhida.Length];
-        for (int i = 0; i < letrasEncontradas.Length; i++)
-            letrasEncontradas[i] = '_';
-
-        List<char> letrasChutadas = new List<char>();
-        int quantidadeErros = 0;
-        bool jogadorAcertou = false;
-        bool jogadorEnforcou = false;
-
-        do
+        public static void Main()
         {
-            // Desenho da forca
-            string cabecaDoBoneco = quantidadeErros >= 1 ? " o " : " ";
-            string tronco = quantidadeErros >= 2 ? "x" : " ";
-            string troncoBaixo = quantidadeErros >= 2 ? " x " : " ";
-            string bracoEsquerdo = quantidadeErros >= 3 ? "/" : " ";
-            string bracoDireito = quantidadeErros >= 4 ? "\\" : " ";
-            string pernas = quantidadeErros >= 5 ? "/ \\" : " ";
-
-            Console.Clear();
-            Console.WriteLine("----------------------------------------------");
-            Console.WriteLine("Jogo da Forca");
-            Console.WriteLine("----------------------------------------------");
-            Console.WriteLine(" ___________        ");
-            Console.WriteLine(" |/        |        ");
-            Console.WriteLine(" |        {0}       ", cabecaDoBoneco);
-            Console.WriteLine(" |        {0}{1}{2} ", bracoEsquerdo, tronco, bracoDireito);
-            Console.WriteLine(" |        {0}       ", troncoBaixo);
-            Console.WriteLine(" |        {0}       ", pernas);
-            Console.WriteLine(" |                  ");
-            Console.WriteLine(" |                  ");
-            Console.WriteLine("_|____              ");
-            Console.WriteLine("----------------------------------------------");
-            Console.WriteLine("Categoria escolhida: " + categoriaEscolhida);
-            Console.WriteLine("Erros do jogador: " + quantidadeErros);
-            Console.WriteLine("Letras já chutadas: " + string.Join(", ", letrasChutadas));
-            Console.WriteLine("----------------------------------------------");
-            Console.WriteLine("Palavra escolhida: " + new string(letrasEncontradas));
-            Console.WriteLine("----------------------------------------------");
-
-            Console.Write("Digite uma letra ou tente adivinhar a palavra inteira: ");
-            string tentativa = Console.ReadLine().ToUpper();
-
-            if (tentativa.Length > 1)
-            {
-                if (tentativa == palavraEscolhida)
-                {
-                    jogadorAcertou = true;
-                    break;
-                }
-                else
-                {
-                    quantidadeErros++;
-                }
-            }
-            else
-            {
-                char chute = tentativa[0];
-                if (letrasChutadas.Contains(chute))
-                {
-                    Console.WriteLine("Você já chutou essa letra. Pressione Enter para continuar.");
-                    Console.ReadLine();
-                    continue;
-                }
-
-                letrasChutadas.Add(chute);
-                bool letraFoiEncontrada = false;
-                for (int i = 0; i < palavraEscolhida.Length; i++)
-                {
-                    if (palavraEscolhida[i] == chute)
-                    {
-                        letrasEncontradas[i] = chute;
-                        letraFoiEncontrada = true;
-                    }
-                }
-                if (!letraFoiEncontrada)
-                    quantidadeErros++;
-            }
-
-            if (new string(letrasEncontradas) == palavraEscolhida)
-                jogadorAcertou = true;
-            jogadorEnforcou = quantidadeErros > 5;
-        } while (!jogadorAcertou && !jogadorEnforcou);
-
-        Console.Clear();
-        if (jogadorAcertou)
-            Console.WriteLine("Parabéns! Você acertou a palavra " + palavraEscolhida);
-        else
-            Console.WriteLine("Você perdeu! A palavra era " + palavraEscolhida);
-
-        Console.Write("Jogar novamente? (S/N): ");
-        if (Console.ReadLine().ToUpper() != "S")
-            break;
+            Jogo jogo = new Jogo();
+            jogo.Iniciar();
+        }
     }
-}
 }
